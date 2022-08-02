77f12f05adb2c6faab136d7f523b1cdbe83ea1a3
Georgi Stoyanov <33185290+geopsllc@users.noreply.github.com>
2022-08-02 11:51:06 -0500

    4.0

--- cccomp.bash
+++ cccomp.bash
@@ -22,7 +22,7 @@ ccontrol_completions () {
         COMPREPLY=( $(compgen -W "core self check" -- $cur) )
         ;;
       "remove")
-        COMPREPLY=( $(compgen -W "self" -- $cur) )
+        COMPREPLY=( $(compgen -W "core self" -- $cur) )
         ;;
       "secret")
         COMPREPLY=( $(compgen -W "set clear" -- $cur) )
